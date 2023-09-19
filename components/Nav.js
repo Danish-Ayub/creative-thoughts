0dc60304f6212d37001011bdddb5d3aa745f34a5
Danish Ayoub <danishinreallife@gmail.com>
2023-09-19 19:30:49 +0500

    changes

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,42 @@
+import Link from 'next/link'
+import { auth } from '../utils/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+
 export default function Nav() {
+  const [user, loading] = useAuthState(auth)
+
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className='flex justify-between items-center py-10'>
+      <Link href='/'>
+        <button className='text-lg font-medium'>Creative Minds</button>
+      </Link>
+      <ul className='flex items-center gap-10'>
+        {!user && (
+          <li>
+            <Link
+              href={'/auth/login'}
+              className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'
+            >
+              Join Now
+            </Link>
+          </li>
+        )}
+        {user && (
+          <li className='flex items-center gap-6'>
+            <Link href='/post'>
+              <button className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium'>
+                Post
+              </button>
+            </Link>
+            <Link href='/dashboard'>
+              <img
+                className='w-12 rounded-full cursor-pointer'
+                src={user.photoURL}
+              />
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
   )
 }
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase'
-import { getFireStore } from 'firebase/firestore'
-import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+// import { getAnalytics } from 'firebase/analytics'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyB4rc3qGtdCWd_-2xi8wgH8tyRpY3pLxkg',
-  authDomain: 'creative-thoughts-a3245.firebaseapp.com',
-  projectId: 'creative-thoughts-a3245',
-  storageBucket: 'creative-thoughts-a3245.appspot.com',
-  messagingSenderId: '301389741422',
-  appId: '1:301389741422:web:a2a61fa23241246529557f',
-  measurementId: 'G-729MT3VWZ4',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+// const analytics = getAnalytics(app)
 export const auth = getAuth()
-export const db = getFireStore()
+export const db = getFirestore()
