c713019f8901b739a6732c37516a8870079e785d
Danish Ayoub <danishinreallife@gmail.com>
2023-09-23 17:43:26 +0500

    changes

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,8 +1,8 @@
 import { FcGoogle } from 'react-icons/fc'
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { auth } from '../../utils/firebase'
-import { useRouter } from 'next/router'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 export default function Login() {
